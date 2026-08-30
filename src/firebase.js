5a40942197ae25bf5fb7d19006edac59f94f84e2
ashishkumarbihar2026-alt <ashishkumarbihar2026@gmail.com>
2026-08-30 15:39:15 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 
 // 👇 Firebase Console > Project Settings > General > "Your apps" > SDK config se copy karo
 const firebaseConfig = {
-  apiKey: 'AIzaSyA8ykLyXlhexkwrqUdEm8iJ_yKzv68EyKI',
+  apiKey: 'AIzaSyA8ykLyXlhexkwrqUdEm8iJ_yKzv58EyKI',
   authDomain: 'om-geranal-manager.firebaseapp.com',
   projectId: 'om-geranal-manager',
   storageBucket: 'om-geranal-manager.firebasestorage.app',
