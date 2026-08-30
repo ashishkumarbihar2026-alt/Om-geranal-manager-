e866668bfa57ca2607eb475cb63134aba30b0a48
ashishkumarbihar2026-alt <ashishkumarbihar2026@gmail.com>
2026-08-30 15:30:31 +0530

    Update setupCode.js

--- src/setupCode.js
+++ src/setupCode.js
@@ -1,4 +1,4 @@
 // Ye code sirf tumhe pata hona chahiye.
 // Signup screen par ye code daalne par hi naya account ban sakega.
 // Deploy karne se pehle ise apni marzi ka koi bhi secret word/number bana lo.
-export const SETUP_CODE = 'apna-secret-code-yaha-likho'
+export const SETUP_CODE = '8271776402'
